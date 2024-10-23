9d52d1d02f266fe7848d59b4f2ab3cc9af1a5c21
José Arizaga <jope.arizag@gmail.com>
2024-10-22 23:25:35 -0300

    lab3

--- Lab3.hs
+++ Lab3.hs
@@ -51,3 +51,75 @@ zipWith = \func -> \l1 -> \l2 -> case l1 of {
         y:ys -> func x y:zipWith func xs ys;
     }
 }
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter = \func -> \l -> case l of {
+    [] -> [];
+    x:xs -> case func x of {
+        True -> x:filter func xs;
+        False -> filter func xs;
+    }
+}
+
+and :: [Bool] -> Bool
+and = \l -> case l of {
+    [] -> True;
+    x:xs -> x && and xs;
+}
+
+or :: [Bool] -> Bool
+or = \l -> case l of {
+    [] -> False;
+    x:xs -> x || or xs;
+}
+
+count :: (a->Bool) ->[a]-> N
+count = \func -> \l -> case l of {
+    [] -> O;
+    x:xs -> case func x of {
+        True -> S(count func xs);
+        False -> count func xs;
+    }
+}
+
+any :: (a->Bool) ->[a]->Bool
+any = \func -> \l -> case l of {
+    [] -> False;
+    x:xs -> case func x of {
+        True -> True;
+        False -> any func xs;
+    }
+}
+
+all :: (a->Bool) ->[a]->Bool
+all = \func -> \l -> case l of {
+    [] -> True;
+    x:xs -> case func x of {
+        True -> all func xs;
+        False -> False;
+    }
+}
+
+countt :: (a->Bool) ->[a]-> N
+countt = \func -> \l -> length (filter func l)
+
+anyy :: (a->Bool) ->[a]->Bool
+anyy = \func -> \l -> or (map func l)
+
+alll :: (a->Bool) ->[a]->Bool
+alll = \func -> \l -> and (map func l)
+
+(++) :: [a]->[a]->[a]
+(++) = \l1 -> \l2 -> case l1 of {
+    [] -> [];
+    x:xs -> case l2 of {
+        [] -> [];
+        y:ys -> x:y:xs++ys
+    }
+}
+
+reverse :: [a]->[a]
+reverse = \l -> case l of {
+    [] -> [];
+    x:xs -> reverse xs:x
+}
